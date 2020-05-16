d803a37b08d669cc909dfaa1e3741c84e0caf84f
ROCO27 <53492072+GtzJr27@users.noreply.github.com>
2020-05-15 19:38:51 -0500

    Update login.blade.php

--- resources/views/front/login.blade.php
+++ resources/views/front/login.blade.php
@@ -5,10 +5,6 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-    <meta name="description" content="Chameleon Admin is a modern Bootstrap 4 webapp &amp; admin dashboard html template with a large number of components, elegant design, clean and organized code.">
-    <meta name="keywords" content="admin template, Chameleon admin template, dashboard template, gradient admin template, responsive admin template, webapp, eCommerce dashboard, analytic dashboard">
-    <meta name="author" content="ThemeSelect">
     <title>@yield('title')</title>
     <link rel="apple-touch-icon" href="/theme-assets/images/ico/apple-icon-120.png">
     <link rel="shortcut icon" type="image/x-icon" href="/theme-assets/images/ico/favicon.ico">
@@ -20,30 +16,14 @@
     <!-- BEGIN CHAMELEON  CSS-->
     <link rel="stylesheet" type="text/css" href="/theme-assets/css/app-lite.css">
     <!-- END CHAMELEON  CSS-->
-    <!-- BEGIN Page Level CSS-->
-    <link rel="stylesheet" type="text/css" href="/theme-assets/css/core/menu/menu-types/vertical-menu.css">
     <link rel="stylesheet" type="text/css" href="/theme-assets/css/core/colors/palette-gradient.css">
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
-    integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
-    crossorigin=""/>
-    <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
-    integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
-    crossorigin=""></script>
-    @yield('styles')
-    <!-- END Page Level CSS-->
-    <!-- BEGIN Custom CSS-->
     <!-- END Custom CSS-->
   </head>
   <body>
 
-
-    <!-- ////////////////////////////////////////////////////////////////////////////-->
-
     <section id="wrapper" class="login-register login-sidebar " style="background-image:url(assets/images/backgrounds/LoginDelivery-02.png);background-size: cover; height: 100vh">
         <div class="login-box"> <!--class="login-box card shadow-none" -->
             <div class="card-body m-100" >
-
-
                 <div class="col-lg-4 col-md-6 col-10  p-0">
                     <div class="  px-1 py-1 m-0">
                         <div class="card-header border-0">
@@ -96,28 +76,8 @@
             </div>
         </div>
     </section>
-    <!-- ////////////////////////////////////////////////////////////////////////////-->
-
-
-
-    @yield('scripts')
-    <script src="/js/app.js" type="text/javascript"></script>
-    <!-- BEGIN VENDOR JS-->
-    <script src="/theme-assets/vendors/js/vendors.min.js" type="text/javascript"></script>
-    <!-- BEGIN VENDOR JS-->
-    <!-- BEGIN PAGE VENDOR JS-->
-    <script src="/theme-assets/vendors/js/charts/chart.min.js" type="text/javascript"></script>
-    <!-- END PAGE VENDOR JS-->
-    <!-- BEGIN CHAMELEON  JS-->
-    <script src="/theme-assets/js/core/app-menu-lite.js" type="text/javascript"></script>
-    <script src="/theme-assets/js/core/app-lite.js" type="text/javascript"></script>
-    <!-- END CHAMELEON  JS-->
-    <!-- BEGIN PAGE LEVEL JS-->
-    <script src="/theme-assets/js/scripts/charts/chartjs/bar/column.js" type="text/javascript"></script>
-    <script src="/theme-assets/js/scripts/charts/chartjs/bar/bar.js" type="text/javascript"></script>
-    <script src="/theme-assets/js/scripts/charts/chartjs/line/line.js" type="text/javascript"></script>
-    <script src="/theme-assets/js/scripts/charts/chartjs/pie-doughnut/pie-simple.js" type="text/javascript"></script>
-    <script src="/theme-assets/js/scripts/charts/chartjs/pie-doughnut/doughnut-simple.js" type="text/javascript"></script>
-    <!-- END PAGE LEVEL JS-->
   </body>
 </html>
+
+
+
