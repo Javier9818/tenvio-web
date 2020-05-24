fc21930858fa7aaa53661816420beaf88dd5cfe4
Jairo Navez <jairowolf123@hotmail.com>
2020-05-23 23:14:53 -0500

    al fin me sale el storage :(

--- app/Http/Controllers/ProductosController.php
+++ app/Http/Controllers/ProductosController.php
@@ -49,7 +49,7 @@ class ProductosController extends Controller
 			else
 				Producto::editar($id, $nombre, $descripcion, $precio, $fotosubida, $categorias_menu_id);
 			if ($sesubiofoto == true)
-				ExtrasController::moverFoto($fotosubida);
+				ExtrasController::moverFotoProducto($fotosubida);
 		}
 		//return 4949;
 		//return abort(409);
