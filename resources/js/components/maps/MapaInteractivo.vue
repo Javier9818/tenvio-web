b0d1dda27c740e2987c287fca6ba73b9573e585a
Javier9818 <53130037+Javier9818@users.noreply.github.com>
2020-05-26 22:18:05 -0500

    Componente de geolocalizacion y ubicación

---
+++ resources/js/components/maps/MapaInteractivo.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="mapaInteractivo">
+        <div id="map" :style="'height:'+height+'; width:' + width"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['height', 'width'],
+        mounted() {
+            this.initMap();
+            console.log(this.marker.getLatLng().lng);
+        },
+        data(){
+            return{
+                map: null,
+                tileLayer: null,
+                layers: [],
+                marker: L.marker([0,0])
+            }
+        },
+        methods:{
+            initMap() {
+                this.map = L.map('map');
+                this.tileLayer =L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                }).addTo(this.map);
+                this.map.locate({setView: true, maxZoom: 17});
+                this.map.on('locationfound', (e) => {this.createMarker(e.latlng);});
+                this.map.on('click', (e) => { this.marker.removeFrom(this.map); this.createMarker(e.latlng);});
+            },
+            createMarker: function(LatLng){
+                this.marker = L.marker(LatLng, {draggable:'true', title: 'Mi ubicación'}).on('dragend', (event) => {
+                    var marker = event.target;
+                    var position = marker.getLatLng();
+                    marker.setLatLng(new L.LatLng(position.lat, position.lng),{draggable:'true'});
+                    marker.bindPopup("Esta es mi ubicación").openPopup();
+                    this.map.panTo(new L.LatLng(position.lat, position.lng))
+                    this.$emit('geoPosition', position);
+                });
+                this.marker.addTo(this.map);
+                this.marker.bindPopup("Esta es mi ubicación").openPopup();
+                this.$emit('geoPosition', this.marker.getLatLng());
+            },
+            initLayers() {
+                this.layers.forEach((layer) => {
+                    const markerFeatures = layer.features.filter(feature => feature.type === 'marker');
+                    const polygonFeatures = layer.features.filter(feature => feature.type === 'polygon');
+
+                    markerFeatures.forEach((feature) => {
+                    feature.leafletObject = L.marker(feature.coords)
+                        .bindPopup(feature.name);
+                    });
+
+                    polygonFeatures.forEach((feature) => {
+                    feature.leafletObject = L.polygon(feature.coords)
+                        .bindPopup(feature.name);
+                    });
+                });
+            },
+            layerChanged(layerId, active) {
+                const layer = this.layers.find(layer => layer.id === layerId);
+                layer.features.forEach((feature) => {
+                    if (active) feature.leafletObject.addTo(this.map);
+                    else feature.leafletObject.removeFrom(this.map);
+                });
+            },
+        }
+    }
+</script>
+
+<style scoped>
+.mapaInteractivo{
+    height: 100%;
+}
+</style>
---
+++ resources/js/components/transporte/Transporte.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <mapa-interactivo height='75vh' width='100%' @geoPosition='geoPosition' ></mapa-interactivo>
+        {{location}}
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+        data(){
+            return{
+                location:null
+            }
+        },
+        methods:{
+            geoPosition(data){
+                this.location = data;
+            }
+        }
+    }
+</script>
