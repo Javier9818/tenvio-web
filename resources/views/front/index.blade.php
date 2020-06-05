3c43a2e7b065da2855e0f6f9cbf3d64e76b653d7
Christian Ferrer <cferrercava@gmail.com>
2020-06-05 16:20:45 -0500

    s

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -58,7 +58,7 @@
       @foreach ($data as $categoria)
       <div class="col-sm-12 col-md-6 col-lg-4">
         <div class="card" style="width: 18rem;">
-          <img class="card-img-top" src="/storage/categories/{{str_replace(' ','',$categoria->categoria)}}.jpg" alt="Card image cap">
+          <img class="card-img-top" src="/storage/categories/{{$categoria->categoria}}.jpg" alt="Card image cap">
           <div class="card-body">
             <h5 class="card-title">{{$categoria->categoria}}</h5>
             <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
