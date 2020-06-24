383dd5f28f5a4e0aef7674753481ec7157b9a030
ROCO27 <53492072+GtzJr27@users.noreply.github.com>
2020-06-24 01:49:57 -0500

    Update front.blade.php

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-  <meta name="description" content="Babette">
+  <meta name="description" content="TE ENVIO - NORTEC">
   <link href="assets/images/favicon/favicon.png" rel="icon">
   <title>@yield('title')</title>
   <link rel="stylesheet"
