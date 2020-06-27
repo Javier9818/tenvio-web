4a934e61be7dff851adc8efc3ff1216cfe3979af
Jairo Navez <jairowolf123@hotmail.com>
2020-06-26 22:30:18 -0500

    los usuarios ya pueden registrar su voucher de pago

--- app/Pagos.php
+++ app/Pagos.php
@@ -1,8 +1,8 @@
 <?php
-
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Pagos extends Model
 {
@@ -10,9 +10,23 @@ class Pagos extends Model
 	protected $fillable = [
 		'id',
 		'empresa_id',
+		'contratos_id',
+		'urlfoto',
 		'monto',
 		'estado',
+		'fecha_pago',
 		'created_at',
 		'updated_at'
 	];
+
+	public static function registrarPago($empresa_id, $contratos_id, $urlFotoVoucher, $monto){
+		return Pagos::create([
+			'empresa_id' => $empresa_id,
+			'contratos_id' => $contratos_id,
+			'urlfoto' => $urlFotoVoucher,
+			'monto' => $monto,
+			'estado' => 'EN ESPERA A APROBAR',
+			'fecha_pago' => Carbon::now()
+		]);
+	}
 }
