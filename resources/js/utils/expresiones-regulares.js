7adb1e227c5d89e883901a8f3d4334e3d388a001
Jairo Navez <jairowolf123@hotmail.com>
2020-09-01 01:33:20 -0500

    un pequeño vuelidate :3

--- resources/js/utils/expresiones-regulares.js
+++ resources/js/utils/expresiones-regulares.js
@@ -3,4 +3,9 @@ import {helpers} from 'vuelidate/lib/validators'
 export const text = helpers.regex('alpha', /^[a-zA-Z0-9À-ÿ.\u00f1\u00d1\s]*$/)
 export const nombreText = helpers.regex('alpha', /^[a-zA-ZÀ-ÿ\u00f1\u00d1\s]*$/)
 
+export const nombreProducto = helpers.regex('alpha', /^[a-zA-Z0-9À-ÿ\+\-\/\u00f1\u00d1\s]*$/)
+export const nombreProductoDescripcion = helpers.regex('alpha', /^[a-zA-Z0-9À-ÿ'\+\-\/\u00f1\u00d1\s]*$/)
+export const nombreCategoria = nombreText
+
+
 export default text
